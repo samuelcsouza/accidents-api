c16badcc0fabd3dd596b2c5c53b49515f088c2a6
samuelcsouza <samuel_dacruz@hotmail.com>
2023-02-27 10:44:54 -0300

    feat: add mongo functions on start

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from socketify_app import app
 import routes
+from database import get_mongo, close_connection
 
 app.listen(3000, lambda config: print("Listening on port http://localhost:%d now\n" % config.port))
+
+@app.on_start
+def on_start():
+    get_mongo()
+
+
+@app.on_shutdown
+def on_stop():
+    close_connection()
+
 
 app.run()
